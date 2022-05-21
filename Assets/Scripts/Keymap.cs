4b77a348752a1ab1f7d06adf178e913b4d15ef44
hucota7 <hucota7@gmail.com>
2022-05-21 09:34:29 -0400

    Keymap for Luke

---
+++ Assets/Scripts/Keymap.cs
@@ -0,0 +1,13 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public static class Keymap
+{
+	public static bool LukeMode = false;
+
+	public static KeyCode Left	=> LukeMode ? KeyCode.S : KeyCode.A;
+	public static KeyCode Right	=> LukeMode ? KeyCode.T : KeyCode.D;
+	public static KeyCode Up	=> LukeMode ? KeyCode.R : KeyCode.W;
+	public static KeyCode Down	=> LukeMode ? KeyCode.H : KeyCode.S;
+}
