f2f52d5b5a3d0c8562c152fa08d326a1d94a4af6
hucota7 <hucota7@gmail.com>
2022-05-22 21:17:19 -0400

    Update Keymap.cs

--- Assets/Scripts/Keymap.cs
+++ Assets/Scripts/Keymap.cs
@@ -4,10 +4,12 @@ using UnityEngine;
 
 public static class Keymap
 {
-	public static bool LukeMode = true;
+	public static bool LukeMode = false;
 
 	public static KeyCode Left	=> LukeMode ? KeyCode.S : KeyCode.A;
 	public static KeyCode Right	=> LukeMode ? KeyCode.T : KeyCode.D;
 	public static KeyCode Up	=> LukeMode ? KeyCode.R : KeyCode.W;
 	public static KeyCode Down	=> LukeMode ? KeyCode.H : KeyCode.S;
+	public static KeyCode Possess => LukeMode ? KeyCode.Space : KeyCode.Space;
+	public static KeyCode Use	=> LukeMode ? KeyCode.A : KeyCode.E;
 }
