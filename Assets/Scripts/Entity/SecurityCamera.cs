69aa890cf622816656b800ee6c5d058de727a2a1
Keldingull <44861659+Kelztergit@users.noreply.github.com>
2022-05-24 21:49:08 +0000

    some new SFX
    no need to use all

--- Assets/Scripts/Entity/SecurityCamera.cs
+++ Assets/Scripts/Entity/SecurityCamera.cs
@@ -5,4 +5,9 @@ using UnityEngine;
 public class SecurityCamera : Entity
 {
     public Transform pivot;
+
+    //disable security camera's animation/rotation when posessed?
+    //or turn off the camera's ability to sense you some other way?
+    //or hop into the security camera's camera-view?
+    //or manually control camera's view in some way.
 }
