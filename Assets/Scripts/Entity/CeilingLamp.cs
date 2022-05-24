1c43ab5510563e1ebd43c4ecfcca24ce947dbca7
Keldingull <44861659+Kelztergit@users.noreply.github.com>
2022-05-24 00:44:44 +0000

    CeilingLamp
    prefabbed up

---
+++ Assets/Scripts/Entity/CeilingLamp.cs
@@ -0,0 +1,10 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class CeilingLamp : Entity       
+{
+    public bool isOn;
+    public Light ceilingLight;
+    
+}
