7858fc457b81393e7bfe7195c3812020476ba784
hucota7 <hucota7@gmail.com>
2022-05-26 07:20:26 -0400

    fixed broken prefab

--- Assets/Scripts/Entity/SecurityCamera.cs
+++ Assets/Scripts/Entity/SecurityCamera.cs
@@ -6,23 +6,44 @@ using Helpers.Tuple;
 public class SecurityCamera : Entity
 {
     public Transform pivot;
-	[SerializeField] Vector3[] rotationSequence; // x is yaw degrees, y is degrees/sec to change, z is seconds to delay before moving on
+	[SerializeField] Vector3[] rotationSequence; // x is yaw degrees, y is rotation duration, z is seconds to delay before moving on
 
-	int sequenceIndex = 0;
-	float sequenceTime = 0;
+	float[] durations;
+	float totalDuration;
 
-    //disable security camera's animation/rotation when posessed?
-    //or turn off the camera's ability to sense you some other way?
-    //or hop into the security camera's camera-view?
-    //or manually control camera's view in some way.
+	float yaw = 0;
 
-    //cam standby beeping sound
-    // AudioManager.Play("CamBeepSFX"); 
+	//disable security camera's animation/rotation when posessed?
+	//or turn off the camera's ability to sense you some other way?
+	//or hop into the security camera's camera-view?
+	//or manually control camera's view in some way.
+
+	//cam standby beeping sound
+	// AudioManager.Play("CamBeepSFX"); 
+
+	public override void Awake()
+	{
+		base.Awake();
+
+		durations = new float[rotationSequence.Length];
+
+		for (int i = 0; i < durations.Length; i++)
+		{
+			durations[i] = rotationSequence[i].y + rotationSequence[i].z;
+		}
+
+		totalDuration = 0;
+		foreach (float d in durations)
+			totalDuration += d;
+	}
 
 	void RotateCamera()
 	{
-		(float yaw, float rate, float wait) operation = rotationSequence[sequenceIndex].Tuple();
+		//float t = Time.time % totalDuration;
+		//int sequenceIndex = ;
+		//(float yaw, float rate, float wait) operation = rotationSequence[sequenceIndex].Tuple();
 
-
+		
+		pivot.localRotation = Quaternion.AngleAxis(yaw, Vector3.up);
 	}
 }
